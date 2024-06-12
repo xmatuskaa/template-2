90f3da8f9e21ea9ed0a5c7221d52c0bf06dac12e
Jan Matuska <xmatuska@mendelu.cz>
2024-06-12 12:25:38 +0200

    corect shooting

--- components/bullet.js
+++ components/bullet.js
@@ -1,38 +1,10 @@
 AFRAME.registerComponent("bullet", {
   init: function () {
-    this.velocity = new CANNON.Vec3(0, 0, -10);
-    this.el.addEventListener("body-loaded", () => {
-      this.el.body.velocity.set(
-        this.velocity.x,
-        this.velocity.y,
-        this.velocity.z
-      );
-    });
-  },
-  tick: function () {
-    if (this.velocity !== null && this.el.body) {
-            const sceneEl = this.el.sceneEl;
-    const cameraEl = sceneEl.querySelector("[camera]");
-      const cameraPosition = new THREE.Vector3();
-      const cameraDirection = new THREE.Vector3();
-
-      // Get the camera's position and direction
-      cameraEl.object3D.getWorldPosition(cameraPosition);
-      cameraEl.object3D.getWorldDirection(cameraDirection);
-        const speed = 30;
-        console.log(cameraDirection);
-      this.el.body.velocity.set(
-        cameraDirection.x * speed,
-        cameraDirection.y * speed,
-        cameraDirection.z * speed
-      );
-    }
-  },
+  }
 });
 
 AFRAME.registerComponent("shooter", {
   init: function () {
-    // Listen for the 'v' key press
     window.addEventListener("keydown", (event) => {
       if (event.key === "v" || event.key === "V") {
         this.shootBullet();
@@ -46,11 +18,9 @@ AFRAME.registerComponent("shooter", {
     const cameraPosition = new THREE.Vector3();
     const cameraDirection = new THREE.Vector3();
 
-    // Get the camera's position and direction
     cameraEl.object3D.getWorldPosition(cameraPosition);
     cameraEl.object3D.getWorldDirection(cameraDirection);
 
-    // Set bullet attributes
     bulletEl.setAttribute("radius", "0.1");
     bulletEl.setAttribute("color", "#FF0000");
     bulletEl.setAttribute(
@@ -59,9 +29,17 @@ AFRAME.registerComponent("shooter", {
     );
     bulletEl.setAttribute("dynamic-body", "mass: 0.3");
     bulletEl.setAttribute("bullet", "");
-
-    // Append bullet to the scene
     sceneEl.appendChild(bulletEl);
 
+    bulletEl.addEventListener("body-loaded", () => {
+      setTimeout(() => {
+        const speed = -30;
+        bulletEl.body.velocity.set(
+          cameraDirection.x * speed,
+          cameraDirection.y * speed,
+          cameraDirection.z * speed
+        );
+      }, 100); 
+    });
   },
 });
